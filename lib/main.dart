6331cbceef237605d18f6bf97116ac3397b953ed
shabeer <muhmmdshabeer@gmail.com>
2025-05-29 10:42:14 +0530

    initial commit production

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SecureVaultInitial(),
+    );
+  }
+}
+
+class SecureVaultInitial extends StatelessWidget {
+  const SecureVaultInitial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Secure Vault in Prod Mode"),),
+    );
+  }
+}
