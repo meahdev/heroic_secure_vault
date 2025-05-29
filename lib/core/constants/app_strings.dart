2d4f4b6a521cde5e464bb629a8131905bea13207
shabeer <muhmmdshabeer@gmail.com>
2025-05-29 10:53:20 +0530

    setup clean architecture base structure

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,19 @@
+class AppStrings {
+  static const create4DigitPassword = "Create 4 Digit Password";
+  static const confirmPassword = "Confirm Password";
+  static const cancel = "Cancel";
+  static const ok = "OK";
+  static const biometricAuthenticationEnabled =
+      "Biometric authentication enabled!";
+  static const biometricAuthenticationFallback =
+      "Couldn't verify with biometrics, but your PIN unlocked the app.";
+  static const enterPin = "Enter PIN";
+  static const inCorrectPassword = "Incorrect Password";
+  static const reinstallAppInstruction =
+      "To reset your PIN, you will need to uninstall and reinstall the application.";
+  static const securityVault = "Security Vault";
+  static const passwordDoesNotMatch = "Password does not match";
+  static const passwordInstruction =
+      "You will be asked to enter the password every time you try"
+      "to access a 'Secure Vault' service.Please make sure you create a memorable password that is hard to guess for others.";
+}
