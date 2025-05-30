f6685c9b0037b46dc3ac8da404f0d4128daf732c
shabeer <muhmmdshabeer@gmail.com>
2025-05-30 05:41:00 +0530

    implemented theme toggling in credential screen

--- lib/features/credential/presentation/screen/credential_screen.dart
+++ lib/features/credential/presentation/screen/credential_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secure_vault/core/constants/app_strings.dart';
+import 'package:secure_vault/shared/widgets/custom_text/custom_text.dart';
+
+import '../../../../core/theme/theme_cubit.dart';
 
 class CredentialScreen extends StatefulWidget {
   const CredentialScreen({super.key});
@@ -10,6 +15,20 @@ class CredentialScreen extends StatefulWidget {
 class _CredentialScreenState extends State<CredentialScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Credential"));
+    final isDark = context.watch<ThemeCubit>().state == ThemeMode.dark;
+    return Scaffold(
+      appBar: AppBar(
+        title: CustomText(AppStrings.yourCredentials),
+        actions: [
+          IconButton(
+            icon: Icon(isDark ? Icons.light_mode : Icons.dark_mode),
+            onPressed: () {
+              context.read<ThemeCubit>().toggleTheme();
+            },
+          ),
+        ],
+      ),
+      body: Center(child: Text("Credential")),
+    );
   }
 }
